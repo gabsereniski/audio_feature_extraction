cb82e5a64a642de05579cfb524ee6bfc2e43c03c
Reginaldo-Neto <reginaldoneto@alunos.utfpr.edu.br>
2023-07-02 15:20:47 -0300

    Extração das características funcionando com paralelização.

---
+++ exampleMLP.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Exemplo do Algoritmo k-NN para scikit-learn.
+@author: diegobertolini
+"""
+import sys
+import numpy as np
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn import tree
+
+
+def main():
+    # load data
+    print("Loading data...")
+    # tr = np.loadtxt(dataTr) ;
+    # ts = np.loadtxt(dataTs) ;
+    tr = np.loadtxt("treino5x5.txt", delimiter=',')
+    ts = np.loadtxt("teste5x5.txt", delimiter=',')
+
+    na = tr.shape[1] - 1  # Assumes the last column is the target variable
+
+    y_test = ts[:, na]
+    y_train = tr[:, na]
+    X_train = tr[:, 1:na]
+    X_test = ts[:, 1:na]
+
+    ## k-NN classifier
+    from sklearn.metrics import classification_report
+
+    neigh = KNeighborsClassifier(n_neighbors=1, metric="euclidean")
+    neigh.fit(X_train, y_train)
+    # neigh.score(X_test, y_test)
+    print("KNN")
+    print(classification_report(y_test, neigh.predict(X_test), digits=4))
+
+    ##SVM com Grid search
+    C_range = 2.0 ** np.arange(-5, 15, 2)
+    gamma_range = 2.0 ** np.arange(3, -15, -2)
+    k = ["rbf"]
+    # instancia o classificador, gerando probabilidades
+    srv = svm.SVC(probability=True, kernel="rbf")
+    ss = StandardScaler()
+    pipeline = Pipeline([("scaler", ss), ("svm", srv)])
+
+    param_grid = {"svm__C": C_range, "svm__gamma": gamma_range}
+
+    # faz a busca
+    grid = GridSearchCV(pipeline, param_grid, n_jobs=-1, verbose=True)
+    grid.fit(X_train, y_train)
+
+    # recupera o melhor modelo
+    model = grid.best_estimator_
+    print("SVM")
+    print(classification_report(y_test, model.predict(X_test), digits=4))
+
+    ### MLP
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+    clf = MLPClassifier(
+        solver="lbfgs",
+        alpha=1e-5,
+        hidden_layer_sizes=(500, 500, 500, 500),
+        random_state=1,
+        max_iter=1000
+    )
+    clf.fit(X_train, y_train)
+    print("MLP, lbfgs")
+    print(classification_report(y_test, clf.predict(X_test), digits=4))
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+    clf = MLPClassifier(
+        solver="sgd",
+        alpha=1e-5,
+        hidden_layer_sizes=(500, 500, 500, 500),
+        random_state=1,
+        max_iter=1000
+    )
+    clf.fit(X_train, y_train)
+    print("MLP, sgd")
+    print(classification_report(y_test, clf.predict(X_test), digits=4))
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+    clf = MLPClassifier(
+        solver="adam",
+        alpha=1e-5,
+        hidden_layer_sizes=(500, 500, 500, 500),
+        random_state=1,
+        max_iter=1000
+    )
+    clf.fit(X_train, y_train)
+    print("MLP, adam")
+    print(classification_report(y_test, clf.predict(X_test), digits=4))
+
+
+### Random Forest Classifier
+    X, y = make_classification(
+        n_samples=1000,
+        n_features=4,
+        n_informative=2,
+        n_redundant=0,
+        random_state=0,
+        shuffle=False,
+    )
+    clf = RandomForestClassifier(n_estimators=10000, max_depth=30, random_state=1)
+    clf.fit(X_train, y_train)
+    print("random forest")
+    print(classification_report(y_test, clf.predict(X_test), digits=4))    
+   
+    ## Decision Tree
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(X_train, y_train)
+    print("DT")
+    print(classification_report(y_test, clf.predict(X_test), digits=4))
+    tree.plot_tree(clf)
+
+if __name__ == "__main__":
+    main()
--- extractor.py
+++ extractor.py
@@ -1,69 +1,99 @@
 import os
 import librosa
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.preprocessing import normalize
-
 from audio_features import *
+from sklearn.preprocessing import normalize
+from joblib import Parallel, delayed
+import multiprocessing
 
-hop_length = 512 # no overelap
-frame_size = 512 # analysis window of 23ms
+num_cores = multiprocessing.cpu_count()
+print("Número de núcleos do processador:", num_cores)
+
+hop_length = 512  # no overlap
+frame_size = 512  # analysis window of 23ms
 current_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.join(current_dir, 'audio_samples')
 data = np.array([])
 
 print("running...")
 
-for folder in os.listdir(root_dir):
-    folder_path = os.path.join(root_dir, folder)
+
+def process_audio_file(file_path):
+    label = os.path.basename(os.path.dirname(file_path))
+    
+    y, sr = librosa.load(file_path)
+    S = librosa.stft(y, n_fft=frame_size, hop_length=hop_length)
+    M = np.abs(S)  # vetor de magnitudes
+    f_bins = librosa.fft_frequencies(sr=sr, n_fft=frame_size)  # vetor de frequencias
+
+    features = np.array([])
+
+    mel_spec = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=frame_size, hop_length=hop_length)
+
+    mfccs = librosa.feature.mfcc(S=librosa.power_to_db(mel_spec), n_mfcc=5)
+    s_centroid = spectral_centroid(M, f_bins)
+    s_rolloff = spectral_rolloff(M, f_bins)
+    s_flux = spectral_flux(M)
+    zc = time_domain_zero_crossings(y, frame_size)
+    low_energy_feature = low_energy(y, frame_size)
+
+    for mfcc in mfccs:
+        texture_mean, texture_var = texture_window(mfcc)
+        features = np.append(features, texture_mean)
+        features = np.append(features, texture_var)
+
+    centroid_mean, centroid_var = texture_window(s_centroid)
+    features = np.append(features, centroid_mean)
+    features = np.append(features, centroid_var)
+
+    rolloff_mean, rolloff_var = texture_window(s_rolloff)
+    features = np.append(features, rolloff_mean)
+    features = np.append(features, rolloff_var)
+
+    flux_mean, flux_var = texture_window(s_flux)
+    features = np.append(features, flux_mean)
+    features = np.append(features, flux_var)
+
+    zc_mean, zc_var = texture_window(zc)
+    features = np.append(features, zc_mean)
+    features = np.append(features, zc_var)
+
+    features = np.append(features, low_energy_feature)
+    features = np.append(features, str(label))  # Convert label to string
+
+    return features
+
+
+def process_folder(folder_path):
+    folder_features = []
     for filename in os.listdir(folder_path):
-        label = folder.split('/')[-1]
-        
         audio_path = os.path.join(folder_path, filename)
-        y, sr = librosa.load(audio_path)
-        S = librosa.stft(y, n_fft=frame_size, hop_length=hop_length)
-        M = np.abs(S) # vetor de magnitudes
-        f_bins = librosa.fft_frequencies(sr=sr, n_fft = frame_size) # vetor de frequencias
-        
-        features = np.array([])
-        
-        mel_spec = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=frame_size, hop_length=hop_length)
+        features = process_audio_file(audio_path)
+        folder_features.append(features)
+    print(f"finished {folder_path}")
+    return folder_features
 
-        mfccs = librosa.feature.mfcc(S=librosa.power_to_db(mel_spec), n_mfcc=5)
-        s_centroid = spectral_centroid(M, f_bins)
-        s_rolloff = spectral_rolloff(M, f_bins)
-        s_flux = spectral_flux(M)
-        zc = time_domain_zero_crossings(y, frame_size)
-        low_energy_feature = low_energy(y, frame_size)
 
-        for mfcc in mfccs:
-            texture_mean, texture_var = texture_window(mfcc)
-            features = np.append(features, texture_mean)
-            features = np.append(features, texture_var)
+# Collect the results from the worker processes
+results = Parallel(n_jobs=num_cores)(delayed(process_folder)(os.path.join(root_dir, folder)) for folder in os.listdir(root_dir))
 
-        centroid_mean, centroid_var = texture_window(s_centroid)
-        features = np.append(features, centroid_mean)
-        features = np.append(features, centroid_var)
+# Flatten the results list
+folder_features = [item for sublist in results for item in sublist]
 
-        rolloff_mean, rolloff_var = texture_window(s_rolloff)
-        features = np.append(features, rolloff_mean)
-        features = np.append(features, rolloff_var)
+# Convert the features list to numpy array
+data = np.array(folder_features)
 
-        flux_mean, flux_var = texture_window(s_flux)
-        features = np.append(features, flux_mean)
-        features = np.append(features, flux_var)
+# Check if data is 1-dimensional
+if data.ndim == 1:
+    features = data  # Just an array of features
+else:
+    features = data[:, :-1]  # All columns except the last
+    labels = data[:, -1]  # Last column
 
-        zc_mean, zc_var = texture_window(zc)
-        features = np.append(features, zc_mean)
-        features = np.append(features, zc_var)
+# Normalize only the features
+normalized_features = normalize(features, axis=0)
 
-        features = np.append(features, low_energy_feature)
-        features = np.append(features, label)
-        if data.size == 0:
-            data = features
-        else:
-            data = np.vstack((data, features))
-    print(f"finished {folder}")
+# Combine the normalized data with the label column
+normalized_data = np.column_stack((normalized_features, labels))
 
-normalized_data = normalize(data, axis=0)
-np.savetxt('features.txt', normalized_data, fmt='%.12f')
+np.savetxt('features.txt', normalized_data, fmt='%s')
